22648b1d2114ad221c32d589bb123e215c0cfbef
codykochmann <kochmanncody@gmail.com>
2016-08-18 09:35:45 -0400

    - added SimpleIOService.py to the repo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SimpleIOService
 This is a Python library that acts as a wrapper for functions to make them into full IO web services with just a few lines of code.
+
+
+# To Do:
+
+- make the SimpleIOService object oriented so it can have start and stop functions
---
+++ SimpleIOService.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# @Author: codykochmann
+# @Date:   2016-08-18 09:33:17
+# @Last Modified 2016-08-18
+# @Last Modified time: 2016-08-18 09:35:19
+
+"""
+This is a really lightweight socket server that
+serves a python function on a network. Basically,
+you have a function that provides an IO service
+that is wrapped in a python function. What this
+does is hosts the function on a specified port
+and allows other devices to use that function to
+process different things.
+
+I specifically built this to make an IO service
+for a raspberry pi network so larger ai jobs can
+be broken into many little parts that each pi can
+do.
+"""
+
+import socket
+import thread
+
+def threaded_function_service(conn,input_function):
+    """ takes data in a port and responds with a function output """
+    collected=''
+    while 1:
+        data = conn.recv(256)
+        print 'parsing: {}'.format(data)
+        if data.startswith('submit'):
+            break
+        if data.endswith("\n") and len(data) is 2:
+            data = data[:-2]
+            break
+        if not data:
+            break
+        collected+=data
+    try:
+        reply=input_function(collected)
+    except Exception, e:
+        reply=e
+        pass
+    print "responding with: {}".format(reply)
+    conn.send(reply)
+    conn.close()
+
+def SimpleIOService(input_function, host='', port=8080):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # set up the socket's settings
+    try:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((host, port))
+        s.listen(5)
+    except socket.error as e:
+        print(str(e))
+    # run an infinite loop that will host this service
+    while True:
+        conn, addr = s.accept()
+        print('connected to: '+addr[0]+':'+str(addr[1]))
+        thread.start_new_thread(threaded_function_service,(conn,input_function))
+
+# test code below
+if __name__ == "__main__":
+    def test_function(input_string):
+        """ returns the length of the string given to the function """
+        return "recieved string with the length of: {}".format(len(input_string))
+
+    SimpleIOService(test_function, port=5559)
+
+
